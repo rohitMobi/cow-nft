840745ef2bbb7e5f59c70cc790df94cefff2aeaf
vaishnavi.ingle@indicchain.com <rohit.pandit@indicchain.com>
2022-10-08 11:18:15 +0530

    Some Minor Change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {  Routes, Route, Navigate  } from 'react-router-dom';
 import DashboardComp from './pages/Dashboard';
 
@@ -10,29 +9,6 @@ function App() {
         <Route path="/dashboard" element={<DashboardComp />} />
       </Routes>
     </>
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
->>>>>>> 26f78b3a3eca11a93d3e6a8f951885aafe56c12f
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,21 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-<<<<<<< HEAD
 import { BrowserRouter } from 'react-router-dom';
-=======
->>>>>>> 26f78b3a3eca11a93d3e6a8f951885aafe56c12f
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-<<<<<<< HEAD
     <BrowserRouter>
       <App />
     </BrowserRouter>
-=======
-    <App />
->>>>>>> 26f78b3a3eca11a93d3e6a8f951885aafe56c12f
   </React.StrictMode>
 );
 
